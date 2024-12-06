84ccce1afa23131bbca629c7cceb1a59065e788f
Jay Prakash <jayp7808@gmail.com>
2024-12-07 00:19:27 +0530

    search added

--- Gemfile
+++ Gemfile
@@ -69,3 +69,5 @@ gem "devise", "~> 4.9"
 gem "font-awesome-sass", "~> 6.5.1"
 
 gem "stripe", "~> 13.2"
+
+gem "pagy", "~> 9.3"
--- app/controllers/catagories_controller.rb
+++ app/controllers/catagories_controller.rb
@@ -2,5 +2,11 @@ class CatagoriesController < ApplicationController
     def show
       @catagory = Catagory.find(params[:id])
       @products = @catagory.products
+      if params[:max].present?
+        @products = @products.where("price <= ?", params[:max])
+      end
+      if params[:min].present?
+        @products = @products.where("price >= ?", params[:min])
+      end
     end
   end
